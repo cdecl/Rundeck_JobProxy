213464111238973cacf1c17ba3c9e215a4601cb7
byung kyu kim <cdeclare@gmail.com>
2018-09-03 16:30:16 +0900

    add source commit

---
+++ Controllers/RunController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+
+namespace run_job.Controllers
+{
+    [Route("api/[controller]")]
+    public class Run : Controller
+    {
+        // GET api/Run
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/Run/5
+        [HttpGet("{token}/{jobid}")]
+        public string Get(string token, string jobid)
+        {
+            string host = $"http://localhost:4440/api/26/job/{jobid}/run";
+
+            var client = new RestClient(host);
+            var req = new RestRequest(Method.POST);
+            req.AddHeader("accept", "application/json");
+            req.AddHeader("x-rundeck-auth-token", token);
+
+            var resp = client.Execute(req);
+            return resp.Content;;
+        }
+
+    }
+}
